b81dabcdc21de872fef43b215a7bbb8bcf398b85
Srinidhi Kukunuri <134805088+SrinidhiKss@users.noreply.github.com>
2023-07-23 11:09:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function ageCalculate()
         let inputDate = new Date(myBirth);
         let birthMonth,birthDate,birthYear;
         let birthDetails = {date:inputDate.getDate(),month:inputDate.getMonth()+1,year:inputDate.getFullYear()}
-        if(currentYear % 4 === 0 || (currentYear % 100 === 0 && currentYear % 400 === 0))
+        if((currentYear % 100 == 0 && currentYear % 4 == 0) || (currentYear % 400 == 0))
         {
             months[1] = 29;
         }
@@ -55,7 +55,7 @@ function ageCalculate()
         else
         {
             birthMonth--;
-            let days = months[currentMonth - 2];
+            let days = months[currentMonth - 1];
             birthDate = days + currentDate - birthDetails.date;
             if(birthMonth < 0)
             {
@@ -67,7 +67,7 @@ function ageCalculate()
         selectedDiv1.innerText = birthMonth;
         selectedDiv2.innerText = birthDate;
         selectedDiv3.innerText = 11 - birthMonth; 
-        selectedDiv4.innerText = months[currentMonth] - birthDate;
+        selectedDiv4.innerText = months[currentMonth-1] - birthDate;
         op.style.opacity = '1';
         op1.style.opacity = '1';
     }
